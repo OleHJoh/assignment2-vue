bc7ffbba021532150c81a27fb2d98e6bb62d4ca5
Rie Nyhus <rie.nyhus@no.experis.com>
2022-01-24 18:30:16 +0100

    give some yellow warnings but usersAnswers in QuestionsList can be used

--- src/components/QuestionsList.vue
+++ src/components/QuestionsList.vue
@@ -3,13 +3,34 @@ import {computed} from 'vue'
 import {useStore} from 'vuex'
 import QuestionsListItem from '../components/QuestionsListItem.vue'
 
-    const onSubmit = () => {
+    const usersAnswers = []
 
+    const handleOnClicked = (index, answer) => {
+
+        const found = usersAnswers.findIndex((question => question.index == index))
+        console.log(found)
+        if (found !== -1) {
+            usersAnswers[found].answer = answer
+        } else {
+            usersAnswers.push({
+                index, answer
+            })
+        }
+
+        console.log(usersAnswers)
     }
+
+    const handleOnSubmit = () => {
+        usersAnswers.sort((a, b) => a.index - b.index)
+        console.log(usersAnswers)
+    }  
+
 
 </script>
 <template>
     <ul>
-        <QuestionsListItem />
+        <QuestionsListItem @onClicked="handleOnClicked" />
     </ul>
+    <QuestionsListItem @onSubmit="handleOnSubmit" />
+
 </template>
--- src/components/QuestionsListItem.vue
+++ src/components/QuestionsListItem.vue
@@ -1,27 +1,33 @@
 <script setup>
-import {computed} from 'vue'
+import {computed, ref} from 'vue'
 import {useStore} from 'vuex'
 
     const store = useStore()
     const questions = computed(() => store.state.questions)
     console.log(questions.value)
 
-    defineProps({
-        answer: {
-            type: Object,
-            required: true
-        }
-    })
+    const emit = defineEmits(
+        ["onClicked"],
+        ["onSubmit"]
+    )
 
     const onAnswerClick = (index, answer) => {
+        emit("onClicked", index, answer)
+    }
 
+    const onSubmitClick = () => {
+        emit("onSubmit")
     }
 
 </script>
 <template>
+
     <li v-for="(question, index) in questions" :key="question">
     <p>{{ question.question }}</p>
     <!-- pass in the current index to know for which question the answer is -->
     <button v-for="answer in question.answers" :key="answer" @click="onAnswerClick(index, answer)">{{ answer }}</button>
     </li>
+    
+    <button @click="onSubmitClick">Submit the answers</button>
+
 </template>
